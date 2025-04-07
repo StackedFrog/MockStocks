987811baac60c1da650a47f5556664518f3ffddc
Schwarz Nikolas <nikolas@a-schwarz.info>
2025-04-07 10:57:16 +0200

    Changed time format, added time range function

---
+++ src/service/stocks_service.rs
@@ -0,0 +1,57 @@
+use yahoo_finance_api::{Quote, YahooConnector};
+use chrono::{DateTime, Local};
+use serde::Serialize;
+
+//TODO: Clean up code, add comments
+
+#[derive(Serialize)]
+pub struct LatestQuote {
+    pub symbol: String,
+    pub date: String,
+    pub close_price: f64,
+}
+
+#[derive(Serialize)]
+pub struct QuoteFromRange {
+    pub symbol: String,
+    pub range: String,
+    pub quotes: Vec<Quote>,
+}
+
+pub async fn fetch_latest_quote(symbol: &str) -> Result<LatestQuote, String> {
+    let provider = YahooConnector::new().map_err(|err| format!("Error creating connector: {:?}", err))?;
+
+    let response = provider
+        .get_latest_quotes(symbol, "1d")
+        .await
+        .map_err(|err| format!("Error fetching stock data: {:?}", err))?;
+
+    let quote = response
+        .last_quote()
+        .map_err(|err| format!("Error extracting quote: {:?}", err))?;
+
+    let local_date: DateTime<Local> = DateTime::from(DateTime::from_timestamp(quote.timestamp as i64, 0).unwrap()); 
+
+    Ok(LatestQuote {
+        symbol: symbol.to_string(),
+        date: local_date.to_string(),
+        close_price: quote.close,
+    })
+}
+
+pub async fn fetch_quote_from_timerange(symbol: &str, range: &str) -> Result<QuoteFromRange, String> {
+    let provider = YahooConnector::new().map_err(|err| format!("Error creating connector: {:?}", err))?;
+
+    let response = provider
+        .get_quote_range(symbol, "1d", range)
+        .await
+        .map_err(|err| format!("Error fetching stock data: {:?}", err))?;
+
+    let fetched_quotes = response.quotes().unwrap();
+
+    Ok(QuoteFromRange {
+        symbol: symbol.to_string(),
+        range: range.to_string(),
+        quotes: fetched_quotes,
+    })
+}
