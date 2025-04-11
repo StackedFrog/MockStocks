b8b267996fd0e47a5d2975fa422cd88e80d4610d
Nikolas <nikolas@a-schwarz.info>
2025-04-11 13:27:42 +0200

    added historic quotes

--- src/service/stocks_service.rs
+++ src/service/stocks_service.rs
@@ -1,8 +1,8 @@
 use yahoo_finance_api::{Quote, YahooConnector};
 use chrono::{DateTime, Local};
 use serde::Serialize;
+use time::{OffsetDateTime, format_description::well_known::Rfc3339};
 use futures::future::join_all;
-
 //TODO: Clean up code, add comments, move errs
 
 #[derive(Serialize)]
@@ -16,6 +16,14 @@ pub struct LatestQuote {
 pub struct QuoteFromRange {
     pub symbol: String,
     pub range: String,
+    pub quotes: Vec<Quote>,
+}
+
+#[derive(Serialize)]
+pub struct HistoricQuotes {
+    pub symbol: String,
+    pub start: String,
+    pub end: String,
     pub quotes: Vec<Quote>,
 }
 
@@ -75,3 +83,27 @@ pub async fn fetch_latest_quotes_parallel(symbols: &[&str]) -> Result<Vec<Latest
 
     Ok(quotes)
 }
+
+pub async fn fetch_historic_quotes(symbol: &str, start: &str, end: &str) -> Result<HistoricQuotes, String> {
+    let provider = YahooConnector::new().map_err(|err| format!("Error creating connector: {:?}", err))?;
+
+    let start_offset = OffsetDateTime::parse(start, &Rfc3339)
+        .map_err(|e| format!("Failed to parse start datetime: {}", e))?;
+    let end_offset = OffsetDateTime::parse(end, &Rfc3339)
+        .map_err(|e| format!("Failed to parse start datetime: {}", e))?;
+    let response = provider
+        .get_quote_history(symbol, start_offset, end_offset)
+        .await
+        .map_err(|err| format!("Error fetching stock data: {:?}", err))?;
+    
+    let fetched_quotes = response
+        .quotes()
+        .map_err(|err| format!("Error extracting quotes: {:?}", err))?;
+
+    Ok(HistoricQuotes{
+        symbol: symbol.to_string(),
+        start: start.to_string(),
+        end: end.to_string(),
+        quotes: fetched_quotes,
+    })
+} 
