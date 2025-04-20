4e5423d021afabbb0b32629cb4379db4ae5109d9
Bianca Casetta <biancacasetta@gmail.com>
2025-04-20 14:38:27 +0200

    updated sql scripts

--- sql/init_db.sql
+++ sql/init_db.sql
@@ -6,3 +6,20 @@ CREATE TABLE Users (
     password VARCHAR(50) NOT NULL,
     role USER_TYPE NOT NULL
 );
+
+CREATE TABLE Transactions (
+    transaction_id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL REFERENCES Users(user_id),
+    date DATE NOT NULL,
+    symbol VARCHAR(10) NOT NULL, -- code name of company
+    transaction_type VARCHAR(10) NOT NULL, -- purchase/sale
+    quantity DECIMAL NOT NULL -- amount of shares
+);
+
+CREATE TABLE Holdings (
+    user_id UUID NOT NULL REFERENCES Users(user_id),
+    symbol VARCHAR(10) NOT NULL, -- company code name or 'cash'
+    quantity DECIMAL NOT NULL,
+    last_updated DATE NOT NULL,
+    PRIMARY KEY (user_id, symbol) -- composite key since user + symbol should be unique
+);
