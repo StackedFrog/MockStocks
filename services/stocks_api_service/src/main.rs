2f72c64ca4bc841aa74b98b8710887be66e424fb
SvenLindstrom <svenarne.Lindstroem@gmail.com>
2025-05-05 20:04:07 +0200

    added telemetry to stock api

--- services/stocks_api_service/src/main.rs
+++ services/stocks_api_service/src/main.rs
@@ -1,5 +1,7 @@
 use axum::Router;
 use telemetry::telemetry;
+use ::telemetry::tracing_propegation;
+use tower_http::trace::{DefaultOnResponse, TraceLayer};
 
 mod router;
 mod services;
@@ -9,13 +11,15 @@ async fn main() {
 
     telemetry::init_telemetry("Stocks_Api_Servie");
 
-
     let app = Router::new()
-        .merge(router::routes_stocks::routes());
-
+        .merge(router::routes_stocks::routes())
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(tracing_propegation::propagate_tracing)
+                .on_response(DefaultOnResponse::new().include_headers(true)),
+        );
     let listener = tokio::net::TcpListener::bind("0.0.0.0:4003")
         .await.unwrap();
-
+    println!("server running on 4003");
     axum::serve(listener, app.into_make_service()).await.unwrap();
 }
-
