d7a9048c75c63df089bf8240204eba19db81f4f0
StackedFrog <isaac.prins4@gmail.com>
2025-05-05 11:46:57 +0200

    token error fixing

--- services/auth_service/src/crypt/token.rs
+++ services/auth_service/src/crypt/token.rs
@@ -1,12 +1,18 @@
+use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, TokenData, Validation};
+use serde::{Deserialize, Serialize};
+
+use super::{Error, Result};
+
 // methods for token 
-pub struct claims {
+#[derive(Serialize, Deserialize)]
+pub struct Claims {
     sub: String,  // subject
     jti: String, // unique id
     exp: u64, // expiration time
     iat:  u64 // creation time 
 }
 
-impl claims {
+impl Claims {
     pub fn new(id: String) -> Self{
         // define token creation
         // generate jti w uuid 
@@ -16,16 +22,16 @@ impl claims {
     }
 }
 
-pub fn validateSignature(token: String) -> Result<String>{
+pub fn validate_signature(token: String) -> Result<TokenData<Claims>>{
     let secret = "secret";
     decode::<Claims>(&token, &DecodingKey::from_secret(secret.as_ref()), &Validation::new(Algorithm::HS256))
-    .map_err(|_| Error::FailedToValidateToken)? // lambda, returns claims or error
+    .map_err(|_| Error::FailedToValidateToken) // lambda, returns claims or error
     // if cannot decode, throw error (fail to validate, mapped into error types)
 }
 
-pub fn createToken(claims: Claims) -> Result<String>{
+pub fn create_token(claims: Claims) -> Result<String>{
     let secret = "secret";
-    let token = encode(&Header::default(), &claims, &EncodingKey::from_secret(secret.as_ref()))
-    .map_err(|_| Error::FailedToCreateToken)?
+    encode(&Header::default(), &claims, &EncodingKey::from_secret(secret.as_ref()))
+    .map_err(|_| Error::FailedToCreateToken)
 }
 
