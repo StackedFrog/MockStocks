73a038ab0de1293c001ad9bada52b18a42be51d5
SvenLindstrom <svenarne.Lindstroem@gmail.com>
2025-05-05 16:42:08 +0200

    added config imple

--- services/auth_service/src/model/mod.rs
+++ services/auth_service/src/model/mod.rs
@@ -1,6 +1,7 @@
 use redis::aio::MultiplexedConnection;
 pub mod redis_token;
 mod error;
+use crate::config::Settings;
 pub use self::error::{Error, Result};
 
 #[derive(Clone, Debug)]
@@ -10,7 +11,8 @@ pub struct ModelManager{
 
 impl ModelManager{
     pub async fn new() -> Self{
-        let client= redis::Client::open("redis://redis:6379").unwrap();
+        let redis_pwd = Settings::get().redis_password.clone();
+        let client= redis::Client::open(format!("redis://:{}@redis:6379", redis_pwd)).unwrap();
         let con = client.get_multiplexed_tokio_connection().await.unwrap();
         Self{client: con}
     }
