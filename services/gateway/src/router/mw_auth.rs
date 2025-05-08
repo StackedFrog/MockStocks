be5f897f28661f6b26f044075d091caa5bbfe34f
SvenLindstrom <svenarne.Lindstroem@gmail.com>
2025-05-08 14:35:12 +0200

    added token validation to gatway

--- services/gateway/src/router/mw_auth.rs
+++ services/gateway/src/router/mw_auth.rs
@@ -1,7 +1,17 @@
 use super::error::{Error, Result};
 use axum::{extract::Request, middleware::Next, response::Response};
 use hyper::HeaderMap;
+use jsonwebtoken::{Algorithm, DecodingKey, TokenData, Validation, decode};
+use serde::{Deserialize, Serialize};
 use shared_utils::ctx::Ctx;
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Claims {
+    pub sub: String,
+    jti: String,
+    pub exp: u64,
+    iat: u64,
+}
 
 pub async fn mw_ctx_resolver(mut req: Request, next: Next) -> Result<Response> {
     let ctx = ctx_resolver(req.headers()).await?;
@@ -12,23 +22,27 @@ pub async fn mw_ctx_resolver(mut req: Request, next: Next) -> Result<Response> {
 }
 
 async fn ctx_resolver(header: &HeaderMap) -> Result<Ctx> {
-    // let token = header.get("AUTHORIZATION").ok_or(Error::Variant1)?;
-
-    let Some(auth_header) = header.get("AUTHORIZATION") else {
-        return Err(Error::TokenMissing);
-    };
+    let auth_header = header.get("AUTHORIZATION").ok_or(Error::TokenMissing)?;
 
     let token = auth_header
         .to_str()
         .ok()
         .and_then(|s| s.strip_prefix("Bearer "))
-        .ok_or(Error::BadTokenFormat);
+        .ok_or(Error::BadTokenFormat)?;
 
-    // validate token -> isaac
+    let token_data = validdate_token(token)?;
 
-    // exstract id from token
-
-    let id = "testID".to_string();
+    let id = token_data.claims.sub;
 
     Ok(Ctx::new(id))
+}
+
+fn validdate_token(token: &str) -> Result<TokenData<Claims>> {
+    let secret = "secret";
+    decode::<Claims>(
+        token,
+        &DecodingKey::from_secret(secret.as_ref()),
+        &Validation::new(Algorithm::HS256),
+    )
+    .map_err(|_| Error::FailedToValidateToken)
 }
