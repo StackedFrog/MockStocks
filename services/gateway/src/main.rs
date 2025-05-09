7b36e27b171e0377f21c308cda97d927fc1dcba7
Nikolas <nikolas@a-schwarz.info>
2025-05-09 14:46:13 +0200

    added cors layer to gateway

--- services/gateway/src/main.rs
+++ services/gateway/src/main.rs
@@ -2,7 +2,10 @@ use axum::{Router, middleware::from_fn};
 use proxy_client::AppState;
 use router::{router_api, router_auth, router_static};
 use telemetry::telemetry;
-use tower_http::trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer};
+use tower_http::{
+    cors::CorsLayer,
+    trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer},
+};
 mod proxy_client;
 mod router;
 mod utils;
@@ -16,6 +19,10 @@ async fn main() {
     let api_router =
         router_api::routes(state.clone()).layer(from_fn(router::mw_auth::mw_ctx_resolver));
 
+    // allowing cors accsess for testing
+    let origin = ["http://localhost:5173".parse().unwrap()];
+    let cors = CorsLayer::new().allow_origin(origin);
+
     let app = Router::new()
         .merge(api_router)
         .merge(router_auth::routes(state))
@@ -24,7 +31,8 @@ async fn main() {
             TraceLayer::new_for_http()
                 .make_span_with(DefaultMakeSpan::new().include_headers(true))
                 .on_response(DefaultOnResponse::new().include_headers(true)),
-        );
+        )
+        .layer(cors);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:4001").await.unwrap();
 
