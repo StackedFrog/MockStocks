eab668fb3e0b89064520478013adf81e70c5fa9c
SvenLindstrom <svenarne.Lindstroem@gmail.com>
2025-05-10 20:04:28 +0200

    gatways config and ctx refactore

--- services/gateway/src/router/mw_auth.rs
+++ services/gateway/src/router/mw_auth.rs
@@ -1,27 +1,52 @@
+use crate::config::Settings;
+
 use super::error::{Error, Result};
 use axum::{extract::Request, middleware::Next, response::Response};
 use hyper::HeaderMap;
 use jsonwebtoken::{Algorithm, DecodingKey, TokenData, Validation, decode};
 use serde::{Deserialize, Serialize};
-use shared_utils::ctx::Ctx;
+use uuid::Uuid;
+//
+// #[derive(Serialize, Deserialize, Clone)]
+// pub struct Claims {
+//     pub sub: String,
+//     jti: String,
+//     pub exp: u64,
+//     iat: u64,
+// }
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Claims {
-    pub sub: String,
-    jti: String,
-    pub exp: u64,
-    iat: u64,
+    pub sub: Uuid, // subject
+    jti: String,   // unique id
+    pub exp: u64,  // expiration time
+    pub role: UserType,
+    iat: u64, // creation time
+}
+#[derive(Debug, Deserialize, Serialize, Clone, Copy)]
+pub enum UserType {
+    Admin,
+    User,
 }
 
 pub async fn mw_ctx_resolver(mut req: Request, next: Next) -> Result<Response> {
     let ctx = ctx_resolver(req.headers()).await?;
+    req.extensions_mut().insert(ctx);
+
+    Ok(next.run(req).await)
+}
+
+pub async fn mw_ctx_resolver_admin(mut req: Request, next: Next) -> Result<Response> {
+    let ctx = ctx_resolver(req.headers()).await?;
+
+    validdate_role(&ctx)?;
 
     req.extensions_mut().insert(ctx);
 
     Ok(next.run(req).await)
 }
 
-async fn ctx_resolver(header: &HeaderMap) -> Result<Ctx> {
+async fn ctx_resolver(header: &HeaderMap) -> Result<Claims> {
     let auth_header = header.get("AUTHORIZATION").ok_or(Error::TokenMissing)?;
 
     let token = auth_header
@@ -32,13 +57,21 @@ async fn ctx_resolver(header: &HeaderMap) -> Result<Ctx> {
 
     let token_data = validdate_token(token)?;
 
-    let id = token_data.claims.sub;
+    let claims = token_data.claims;
 
-    Ok(Ctx::new(id))
+    Ok(claims)
+}
+
+fn validdate_role(claims: &Claims) -> Result<()> {
+    match claims.role {
+        UserType::Admin => {}
+        UserType::User => Err(Error::NotAuthorized)?,
+    };
+    Ok(())
 }
 
 fn validdate_token(token: &str) -> Result<TokenData<Claims>> {
-    let secret = "secret";
+    let secret = &Settings::get().token_secret;
     decode::<Claims>(
         token,
         &DecodingKey::from_secret(secret.as_ref()),
