cb7b23bbf8f93935882c214207c9619d6206bc8c
SvenLindstrom <svenarne.Lindstroem@gmail.com>
2025-05-11 12:23:06 +0200

    added auth context wrapper

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,21 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import { AuthProvider } from './auth_context.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter> 
+  <AuthProvider>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </AuthProvider>
 )
+
+
+  // <StrictMode>
+  // <AuthProvider>
+  //   <BrowserRouter>
+  //     <App />
+  //   </BrowserRouter>
+  // </AuthProvider>
+  // </StrictMode>,
