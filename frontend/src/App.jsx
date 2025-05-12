c354f36910caabf5523793ac8c7e5f71c57c8e72
SvenLindstrom <svenarne.Lindstroem@gmail.com>
2025-05-12 13:03:16 +0200

    misc

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,24 +4,27 @@ import LangingPage from './pages/LangingPage';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import TradingPage from './pages/TradingPage.jsx'
+import AdminPage from './pages/AdminPage.jsx'
 import NotFound from './pages/NotFound';
+import { useAuth } from './auth_context.jsx'
+import UserNavBar from './components/UserNavBar.jsx';
 
 function App() {
-  
+  const { accessToken } = useAuth()
   const location = useLocation()
   const defaultNavPaths = ["/", "/login", "/register"]
 
   const isDefaultPath = defaultNavPaths.includes(location.pathname);
-  
+
 
   return (
     <div>
-
       {/* conditionally render nav bar */}
-      {isDefaultPath ? <NavBar /> : null}
+      {!accessToken ? <NavBar /> : <UserNavBar />}
       <Routes>
         <Route path="/" element={<LangingPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<AdminPage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/trade" element={<TradingPage />} />
         <Route path="*" element={<NotFound />} />
