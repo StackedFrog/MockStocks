01c94085b772058465814610c2f74cbd6ea0258e
SvenLindstrom <svenarne.Lindstroem@gmail.com>
2025-05-12 13:02:39 +0200

    added components

---
+++ frontend/src/components/UserNavBar.jsx
@@ -0,0 +1,19 @@
+import { Link  } from 'react-router-dom'
+import LogoutButton from './LogoutButton'
+
+function UserNavBar() {
+  return (
+  <>
+    <nav className="flex justify-end gap-1 font-bold ">
+      <Link className="p-2 m-1 rounded hover:bg-black hover:text-white"to="/admin">Admin</Link>
+      <Link className="p-2 m-1 rounded hover:bg-black hover:text-white" to="/trade">Trade</Link>
+      <Link className="p-2 m-1 rounded hover:bg-black hover:text-white"to="/profile">Profile</Link>
+      <LogoutButton />
+    </nav>
+  </>
+  )
+}
+
+
+
+export default UserNavBar
