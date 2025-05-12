1d616ae365e5e928cf5a47c6a56d596b32081a4d
SvenLindstrom <svenarne.Lindstroem@gmail.com>
2025-05-12 17:43:54 +0200

    added components

---
+++ frontend/src/components/LogoutEveryWhere.jsx
@@ -0,0 +1,23 @@
+import { useApi } from "./../api_wrapper.jsx"
+import { useNavigate } from 'react-router-dom'
+
+
+function LogoutButtonEveryWhere(){
+
+	const navigate = useNavigate()
+	const {apiUnAuth} = useApi()
+
+	const onLogout = async () => {
+		await apiUnAuth("/auth/user/logout_all")
+		navigate("/")
+	}
+
+        return <button
+          onClick={onLogout}
+          className="bg-blue-500 hover:bg-blue-700 active:bg-blue-600 text-white font-bold py-2 px-4 m-2 rounded"
+        >
+          Logout of all Devices
+	</button>
+}
+
+export default LogoutButtonEveryWhere
