0c75a6fc455b1a135587e2905f54c7e9aa0c297e
Nikolas <nikolas@a-schwarz.info>
2025-05-14 09:43:56 +0200

    merged with frontend branch

---
+++ frontend/src/components/ProfilePicture.jsx
@@ -0,0 +1,10 @@
+function DisplayProfilePicture(){
+    // js 
+    return(
+        <>
+            <img src="" alt= "user's profile picture" />
+        </>
+    )
+}
+
+export default DisplayProfilePicture
