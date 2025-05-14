2b83982ff327441dea48cccc2b15412d05ed2964
Nikolas <nikolas@a-schwarz.info>
2025-05-14 17:00:14 +0200

    addd trending stock cards

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,15 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useApi } from "../api_wrapper.jsx";
+import StockCard from "../components/StockCard.jsx";
 
 function DashboardPage () {
         const {apiFetch} = useApi();
+        const [trendingData, setTrendingData] = useState(null);
 
         useEffect(() => {
                 const fetchTrending = async () => {
                         try {
                                 const response = await apiFetch("/api/stocks_api/trending") ;
                                 const data = await response.json();
-                                console.log(data);
+                                setTrendingData(data.slice(0, 10));
                         } catch (error){
                                 console.error("Could not fetch trending stocks.", error);
                         }
@@ -19,6 +21,11 @@ function DashboardPage () {
 
         return (
                 <div>
+                {trendingData ? (
+                        trendingData.map((stock) => ( <StockCard key={stock} symbol={stock} /> ))
+                ) : (
+                        <p></p>
+                )}
                 </div>
         );
 }
