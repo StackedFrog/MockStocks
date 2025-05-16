38bd5100a9942bf7dbb477d3e9b4cbbf8df0db2d
StackedFrog <isaac.prins4@gmail.com>
2025-05-16 16:50:52 +0200

    merge conflicts

--- frontend/src/pages/logged-in/Account.jsx
+++ frontend/src/pages/logged-in/Account.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { useApi } from "../../hooks/useApi.jsx";
 import SideNav from "../../components/SideNav.jsx";
+import Button from "../../components/ui/Button.jsx";
 
-function DisplayProfile(){
+function DisplayProfile({user}){
     // const {apiFetch} = useApi();
     const name = "";
     const email = "";
@@ -32,17 +33,26 @@ function DisplayProfile(){
 
     return(
         <>
-            <div className = "min-h-screen bg-primary">
-                <SideNav></SideNav>
-                
-                <div className = "flex flex-col justify-start text-text bg-primary">
-                    <div>Name: ${name}</div>
-                    <div>Email: ${email}</div>
-                    <div>Balance: ${cash}</div>
+            <div className = "flex flex-col justify-center align-center bg-background ">
+                <div className="flex justify-start p-6 font-heading">
+                    <p>Account</p>
                 </div>
-                <div className = "flex justify-between text-text bg-primary sm:flex-col sm:justify-start">
-                    <div>Delete account</div>
-                    <div>Log Out</div>
+                <div className="bg-primary rounded-3xl p-8 gap-2 sm:flex-col ">
+                    <div className="flex justify-between">                
+                        <div className = "flex flex-col justify-start text-text bg-primary">
+                            <div>Name: ${name}</div>
+                            <div>Email: ${email}</div>
+                            <div>Balance: ${cash}</div>
+                        </div>
+                        <div className="flex flex-col justify-start text-text ">
+                            <div>edit name</div>
+                            <div>edit email</div>
+                        </div>
+                    </div>
+                    <div className = "flex justify-between text-text bg-primary sm:flex-col sm:justify-center">
+                        <Button className="">Log out all accounts</Button>
+                        <Button className="">Delete profile</Button>
+                    </div>
                 </div>
             </div>
         </>
