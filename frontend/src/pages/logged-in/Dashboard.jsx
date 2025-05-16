5e80072520e886e081729dd148e80fd64e3e527e
Nikolas <nikolas@a-schwarz.info>
2025-05-16 11:16:55 +0200

    adjusted StockCard styling

--- frontend/src/pages/logged-in/Dashboard.jsx
+++ frontend/src/pages/logged-in/Dashboard.jsx
@@ -21,53 +21,19 @@ function DashboardPage () {
 		const fetchHoldings = async () => {
 			const response = await apiFetch("/api/user/holdings");
 			const data = await response.json();
-			if (data.length < 1){ // to be removed :-)
-				setUserHoldings([
-					{
-						"user_id": "2b1a027e-e047-4fd2-a438-08f786e229b8",
-						"symbol": "AAPL",
-						"quantity": "0.2057164613826031792680556118",
-						"last_updated": "2025-05-13T13:32:44.751754Z"
-					},
-					{
-						"user_id": "2b1a027e-e047-4fd2-a438-08f786e229b8",
-						"symbol": "MSFT",
-						"quantity": "0.0043625383157004248818095491",
-						"last_updated": "2025-05-13T13:34:21.257960Z"
-					},
-					{
-						"user_id": "2b1a027e-e047-4fd2-a438-08f786e229b8",
-						"symbol": "GOOG",
-						"quantity": "15.164919014933582157465672891",
-						"last_updated": "2025-05-13T13:41:56.110685Z"
-					},
-					{
-						"user_id": "2b1a027e-e047-4fd2-a438-08f786e229b8",
-						"symbol": "AMZN",
-						"quantity": "5.6183719000819214019632024083",
-						"last_updated": "2025-05-13T13:42:50.411527Z"
-					},
-					{
-						"user_id": "2b1a027e-e047-4fd2-a438-08f786e229b8",
-						"symbol": "META",
-						"quantity": "0.0000392128730043053045414790",
-						"last_updated": "2025-05-13T13:46:39.469774Z"
-					}
-				]) 
-			}
-			// setUserHoldings(data);
+			setUserHoldings(data);
 		}
 		fetchTrending();
 		fetchHoldings();
 	}, [])
 
 	return (
-		<div className="grid grid-cols-14 auto-rows-auto">
+		<div className="grid grid-cols-12 auto-rows-auto bg-background gap-4">
 			<div className="col-span-2"></div>
-			<h1 className="row-start-3 col-start-3 col-span-12">Your Holdings</h1>
+			<h1 className="font-heading text-secondary row-start-3 col-start-2 col-span-10 text-3xl my-10">Your Holdings</h1>
 			{userHoldings &&
                         userHoldings.map((holding, index) => {
-                        	const colStart = 4 + (index % 5) * 2;
+                        	const colStart = 2 + (index % 5) * 2;
                         	const rowStart = 4 + Math.floor(index / 5);
                         	return (
                         		<div
@@ -75,16 +41,16 @@ function DashboardPage () {
                         			style={{ gridColumnStart: colStart, gridRowStart: rowStart }}
                         			className="col-span-2"
                         		>
-                        			<StockCard symbol={holding.symbol} share={holding.quantity} />
+                        			<StockCard symbol={holding.symbol}/>
                         		</div>
                         	);
                         })}
-			<h1 className="row-start-6 col-start-3 col-span-12">Trending Stocks</h1>
+			<h1 className="font-heading text-secondary row-start-6 col-start-2 col-span-10 text-3xl my-10">Trending Stocks</h1>
 
 			{trendingData &&
                         trendingData.map((stock, i) => {
                         	const index = i;
-                        	const colStart = 4 + (index % 5) * 2;
+                        	const colStart = 2 + (index % 5) * 2;
                         	const rowStart = 7 + Math.floor(index / 5);
                         	return (
                         		<div
@@ -92,7 +58,7 @@ function DashboardPage () {
                         			style={{ gridColumnStart: colStart, gridRowStart: rowStart }}
                         			className="col-span-2"
                         		>
-                        			<StockCard symbol={stock} share={0} />
+                        			<StockCard symbol={stock}/>
                         		</div>
                         	);
                         })}
