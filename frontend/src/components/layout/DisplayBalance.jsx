03fec175697bc8708287337f78c2bbbb6411aa21
StackedFrog <isaac.prins4@gmail.com>
2025-05-17 11:49:53 +0200

    simplied display balance and name

--- frontend/src/components/layout/DisplayBalance.jsx
+++ frontend/src/components/layout/DisplayBalance.jsx
@@ -1,32 +1,7 @@
 import React, { useState } from "react"
-import { useApi } from "../../hooks/useApi.jsx"
 
 
-function DisplayBalance(user){
-    const {apiFetch} = useApi()
-    const cash = user.balance ?? "!";
-
-    const handleBalance = async (e) =>{
-        e.preventDefault()
-        try{
-            const response = await apiFetch("/api/user/info",{
-                method: "GET",
-                body: JSON.stringify(cash)
-            })
-            if (response.ok){
-                const data = await response.json();
-                console.log(data)
-            }
-            else{
-                console.error(err)
-                alert("Something went wrong.")
-            }
-        }
-        catch(err){
-            console.error(err)
-            alert("Something went wrong.")
-        }
-    }
+function DisplayBalance({cash}){
     return(
         <>
             <div className = "text-background bg-primary p-2 rounded-lg ">
