da9d273e32b7f405b1a1ca15a257a2e462aa0149
Antoine <gantoine.st@gmail.com>
2025-05-17 14:27:13 +0200

    Search bar fix and real time trades

---
+++ frontend/src/components/trading/StocksSearchBar.jsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { useApi } from '../../hooks/useApi.jsx';
+
+export function StocksSearchBar({ onSelect, initialSymbol }) {
+  const { apiFetch } = useApi();
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const divRef = useRef(null);
+
+  // EVENTLISTENER FOR CLICK OUTSIDE DIV
+  useEffect(() => {
+    // Define the click event handler
+    const handleClickOutside = (event) => {
+      if (divRef.current && !divRef.current.contains(event.target)) {
+        setSearchTerm("")
+      }}
+    document.addEventListener('click', handleClickOutside);
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
+  // FETCH STOCK NAMES
+  useEffect(() => {
+    if (!searchTerm) {
+      setSearchResults([]);
+      return;
+    }
+
+    const fetchStocks = async () => {
+      try {
+        const response = await apiFetch(
+          `/api/stocks_api/ticker?symbol=${encodeURIComponent(searchTerm)}`
+        );
+        if (!response.ok) throw new Error('Network response was not ok');
+        const results = await response.json();
+        setSearchResults(results);
+      } catch (err) {
+        console.error('Error fetching stock names:', err);
+        setSearchResults([]);
+      }
+    };
+
+    const timeout = setTimeout(fetchStocks, 300);
+    return () => clearTimeout(timeout);
+  }, [searchTerm]);
+
+  const handleChange = e => setSearchTerm(e.target.value);
+
+  const handleSelect = (symbol, name) => {
+    // triggers FETCH STOCK NAMES
+    setSearchTerm('');
+    setSearchResults([]);
+    onSelect(symbol, name);
+  };
+
+  return (
+    <div ref={divRef} className="flex justify-center items-center flex-col mt-4 p-4 text-white"
+    >
+      <input
+        type="text"
+        placeholder="Apple..."
+        value={searchTerm}
+        onChange={handleChange}
+        className="z-1 w-full p-3 bg-[#1a1a1a] text-white border border-gray-700 rounded-xl shadow-sm placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent transition-all duration-200"
+      />
+      {searchResults.length > 0 && (
+        <section className="z-10 w-full bg-[#1f1f1f] mt-1 border border-gray-700 rounded-md">
+          {searchResults.map(stock => (
+            <button
+              key={stock.symbol}
+              onClick={() => handleSelect(stock.symbol, stock.name)}
+              onBlur={() => {setSearchTerm("")}}
+              className="w-full text-left p-2 hover:bg-gray-600 text-white"
+            >
+              {stock.name} ({stock.symbol})
+            </button>
+          ))}
+        </section>
+      )}
+    </div>
+  );
+}
+
--- frontend/src/components/trading/TradingChart.jsx
+++ frontend/src/components/trading/TradingChart.jsx
@@ -1,57 +1,155 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
+import { useApi } from '../../hooks/useApi.jsx';
 import { createChart, ColorType, CandlestickSeries } from 'lightweight-charts';
 
-const CandleChart = ({ data, colors = {} }) => {
+
+
+export const TradingChart = ({ symbol, colors }) => {
+  const { apiFetch } = useApi();
+  const [stockName, setStockName] = useState(null);
+  const [stockData, setStockData] = useState([]);
+  const stockDataRef = useRef([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const updateTrades = useCallback(async () => {
+    const data = await fetchTrades()
+
+    const lastTradeAPI = data.slice(-1)[0]
+    const lastTradeData = stockDataRef.current.slice(-1)[0]
+
+    const dataDate = new Date(lastTradeData.time * 1000)
+    const dataMinute = dataDate.getMinutes() % 10
+
+    const apiDate = new Date(lastTradeAPI.time * 1000)
+    const apiMinute = apiDate.getMinutes() % 10
+
+    // same minute?
+    if (dataMinute === apiMinute) {
+      // UPDATE the last trade
+      const newData = [
+        ...stockDataRef.current.slice(0, -1),
+        lastTradeAPI
+      ]
+      
+      setStockData(newData)
+      stockDataRef.current = newData
+    } else {
+      // ADD new Trade
+      const newData = [
+        ...stockDataRef.current,
+        lastTradeAPI
+      ]
+      setStockData(newData)
+      stockDataRef.current = newData
+    }
+
+  }, [stockData])
+
+  const fetchTrades = async () => {
+    try {
+      const response = await apiFetch(
+        `/api/stocks_api/range?symbol=${encodeURIComponent(searchParams.get('symbol'))}&range=12h&interval=15m`
+      )
+      if (!response.ok) throw new Error("Response is not ok: " + response.statusText)
+
+
+      const data = await response.json()
+      const formatted = data.quotes.map(q => ({
+        time: q.timestamp,
+        open: q.open,
+        high: q.high,
+        low: q.low,
+        close: q.close
+      }))
+      return formatted
+    } catch (err) {
+      console.error(`Fetch error in TradingChart component: ${err}`)
+    }
+    return
+  }
+
+
+  useEffect(() => {
+  // initial fetch
+  (async () => {
+    const data = await fetchTrades();
+    setStockData(data);
+    stockDataRef.current = data
+    
+  })();
+
+    // fetches trading data every 300ms
+    const fetchLoop = setInterval(() => {
+      updateTrades()
+    }, 10000);
+
+    // cleanUp fetchLoop on unmount
+    return () => clearInterval(fetchLoop)
+  }, [])
+
+  return <TradingviewApiChart data={stockData} colors={colors} />;
+};
+
+
+const TradingviewApiChart = ({ data, colors = {} }) => {
   const {
     backgroundColor = 'rgb(15,15,15)',
-    textColor         = 'white',
+    textColor       = 'white',
   } = colors;
 
   const chartContainerRef = useRef(null);
+  const chartRef = useRef(null);
+  const seriesRef = useRef(null);
 
   useEffect(() => {
     // 1. Create chart instance
     const chart = createChart(chartContainerRef.current, {
       layout: {
-        background: { type: ColorType.Solid, color: backgroundColor },  // docs: use createChart to set background :contentReference[oaicite:0]{index=0}
+        background: { type: ColorType.Solid, color: backgroundColor },
         textColor,
       },
-      width:  chartContainerRef.current.clientWidth,
+      width: chartContainerRef.current.clientWidth,
       height: 300,
     });
 
-    // 2. Add candlestick series via addSeries(CandlestickSeries, …)
+    // 2. Add candlestick series
     const series = chart.addSeries(CandlestickSeries, {
-      upColor:        '#4fff44',
-      borderUpColor:  '#4fff44',
-      wickUpColor:    '#4fff44',
-      downColor:      '#ff4976',
-      borderDownColor:'#ff4976',
-      wickDownColor:  '#ff4976',
-    });  // per v4+ API use addSeries(CandlestickSeries, options) :contentReference[oaicite:1]{index=1}
+      upColor:         '#4fff44',
+      borderUpColor:   '#4fff44',
+      wickUpColor:     '#4fff44',
+      downColor:       '#ff4976',
+      borderDownColor: '#ff4976',
+      wickDownColor:   '#ff4976',
+    });
 
-    series.setData(data);
+    // 3. Save refs
+    chartRef.current = chart;
+    seriesRef.current = series;
+
+    // 4. Initial fitContent once
     chart.timeScale().fitContent();
 
-    // 3. Make chart responsive
+    // 5. Resize handler
     const handleResize = () => {
       chart.applyOptions({ width: chartContainerRef.current.clientWidth });
     };
     window.addEventListener('resize', handleResize);
 
-    // Cleanup on unmount
+    // 6. Cleanup
     return () => {
       window.removeEventListener('resize', handleResize);
       chart.remove();
     };
-  }, [data, backgroundColor, textColor]);
+  }, [backgroundColor, textColor]);
+
+  // Update only series data — does not affect chart scroll/center
+  useEffect(() => {
+    if (seriesRef.current && data?.length) {
+      seriesRef.current.setData(data);
+    }
+  }, [data]);
 
   return <div ref={chartContainerRef} />;
 };
 
-export const TradingChart = ({ data, symbol, colors, hideChart}) => {
-        if (hideChart.hideChart) return null;
-        return <CandleChart data={data} colors={colors} />;
-};
-
-export default TradingChart;
